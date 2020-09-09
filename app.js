577a4eeb700d7d953bd8549047056e5a457c0d05
single-byte <70197335+single-byte@users.noreply.github.com>
2020-09-10 02:23:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,414 @@
+// alert("Best On Desktop/PC or large screens");
+const count = new Map(); count.set("AD", "Andorra");
+count.set("AE", "United Arab Emirates");
+count.set("AF", "Afghanistan");
+count.set("AG", "Antigua and Barbuda");
+count.set("AI", "Anguilla");
+count.set("AL", "Albania");
+count.set("AM", "Armenia");
+count.set("AO", "Angola");
+count.set("AQ", "Antarctica");
+count.set("AR", "Argentina");
+count.set("AS", "American Samoa");
+count.set("AT", "Austria");
+count.set("AU", "Australia");
+count.set("AW", "Aruba");
+count.set("AX", "Åland Islands");
+count.set("AZ", "Azerbaijan");
+count.set("BA", "Bosnia and Herzegovina");
+count.set("BB", "Barbados");
+count.set("BD", "Bangladesh");
+count.set("BE", "Belgium");
+count.set("BF", "Burkina Faso");
+count.set("BG", "Bulgaria");
+count.set("BH", "Bahrain");
+count.set("BI", "Burundi");
+count.set("BJ", "Benin");
+count.set("BL", "Saint Barthélemy");
+count.set("BM", "Bermuda");
+count.set("BN", "Brunei Darussalam");
+count.set("BO", "Bolivia, Plurinational State of");
+count.set("BQ", "Bonaire, Sint Eustatius and Saba");
+count.set("BR", "Brazil");
+count.set("BS", "Bahamas");
+count.set("BT", "Bhutan");
+count.set("BV", "Bouvet Island");
+count.set("BW", "Botswana");
+count.set("BY", "Belarus");
+count.set("BZ", "Belize");
+count.set("CA", "Canada");
+count.set("CC", "Cocos (Keeling) Islands");
+count.set("CD", "Congo, Democratic Republic of the");
+count.set("CF", "Central African Republic");
+count.set("CG", "Congo");
+count.set("CH", "Switzerland");
+count.set("CI", "Côte d'Ivoire");
+count.set("CK", "Cook Islands");
+count.set("CL", "Chile");
+count.set("CM", "Cameroon");
+count.set("CN", "China");
+count.set("CO", "Colombia");
+count.set("CR", "Costa Rica");
+count.set("CU", "Cuba");
+count.set("CV", "Cabo Verde");
+count.set("CW", "Curaçao");
+count.set("CX", "Christmas Island");
+count.set("CY", "Cyprus");
+count.set("CZ", "Czechia");
+count.set("DE", "Germany");
+count.set("DJ", "Djibouti");
+count.set("DK", "Denmark");
+count.set("DM", "Dominica");
+count.set("DO", "Dominican Republic");
+count.set("DZ", "Algeria");
+count.set("EC", "Ecuador");
+count.set("EE", "Estonia");
+count.set("EG", "Egypt");
+count.set("EH", "Western Sahara");
+count.set("ER", "Eritrea");
+count.set("ES", "Spain");
+count.set("ET", "Ethiopia");
+count.set("FI", "Finland");
+count.set("FJ", "Fiji");
+count.set("FK", "Falkland Islands (Malvinas)");
+count.set("FM", "Micronesia, Federated States of");
+count.set("FO", "Faroe Islands");
+count.set("FR", "France");
+count.set("GA", "Gabon");
+count.set("GB", "United Kingdom of Great Britain and Northern Ireland");
+count.set("GD", "Grenada");
+count.set("GE", "Georgia");
+count.set("GF", "French Guiana");
+count.set("GG", "Guernsey");
+count.set("GH", "Ghana");
+count.set("GI", "Gibraltar");
+count.set("GL", "Greenland");
+count.set("GM", "Gambia");
+count.set("GN", "Guinea");
+count.set("GP", "Guadeloupe");
+count.set("GQ", "Equatorial Guinea");
+count.set("GR", "Greece");
+count.set("GS", "South Georgia and the South Sandwich Islands");
+count.set("GT", "Guatemala");
+count.set("GU", "Guam");
+count.set("GW", "Guinea-Bissau");
+count.set("GY", "Guyana");
+count.set("HK", "Hong Kong");
+count.set("HM", "Heard Island and McDonald Islands");
+count.set("HN", "Honduras");
+count.set("HR", "Croatia");
+count.set("HT", "Haiti");
+count.set("HU", "Hungary");
+count.set("ID", "Indonesia");
+count.set("IE", "Ireland");
+count.set("IL", "Israel");
+count.set("IM", "Isle of Man");
+count.set("IN", "India");
+count.set("IO", "British Indian Ocean Territory");
+count.set("IQ", "Iraq");
+count.set("IR", "Iran, Islamic Republic of");
+count.set("IS", "Iceland");
+count.set("IT", "Italy");
+count.set("JE", "Jersey");
+count.set("JM", "Jamaica");
+count.set("JO", "Jordan");
+count.set("JP", "Japan");
+count.set("KE", "Kenya");
+count.set("KG", "Kyrgyzstan");
+count.set("KH", "Cambodia");
+count.set("KI", "Kiribati");
+count.set("KM", "Comoros");
+count.set("KN", "Saint Kitts and Nevis");
+count.set("KP", "Korea, Democratic People's Republic of");
+count.set("KR", "Korea, Republic of");
+count.set("KW", "Kuwait");
+count.set("KY", "Cayman Islands");
+count.set("KZ", "Kazakhstan");
+count.set("LA", "Lao People's Democratic Republic");
+count.set("LB", "Lebanon");
+count.set("LC", "Saint Lucia");
+count.set("LI", "Liechtenstein");
+count.set("LK", "Sri Lanka");
+count.set("LR", "Liberia");
+count.set("LS", "Lesotho");
+count.set("LT", "Lithuania");
+count.set("LU", "Luxembourg");
+count.set("LV", "Latvia");
+count.set("LY", "Libya");
+count.set("MA", "Morocco");
+count.set("MC", "Monaco");
+count.set("MD", "Moldova, Republic of");
+count.set("ME", "Montenegro");
+count.set("MF", "Saint Martin, (French part)");
+count.set("MG", "Madagascar");
+count.set("MH", "Marshall Islands");
+count.set("MK", "North Macedonia");
+count.set("ML", "Mali");
+count.set("MM", "Myanmar");
+count.set("MN", "Mongolia");
+count.set("MO", "Macao");
+count.set("MP", "Northern Mariana Islands");
+count.set("MQ", "Martinique");
+count.set("MR", "Mauritania");
+count.set("MS", "Montserrat");
+count.set("MT", "Malta");
+count.set("MU", "Mauritius");
+count.set("MV", "Maldives");
+count.set("MW", "Malawi");
+count.set("MX", "Mexico");
+count.set("MY", "Malaysia");
+count.set("MZ", "Mozambique");
+count.set("NA", "Namibia");
+count.set("NC", "New Caledonia");
+count.set("NE", "Niger");
+count.set("NF", "Norfolk Island");
+count.set("NG", "Nigeria");
+count.set("NI", "Nicaragua");
+count.set("NL", "Netherlands");
+count.set("NO", "Norway");
+count.set("NP", "Nepal");
+count.set("NR", "Nauru");
+count.set("NU", "Niue");
+count.set("NZ", "New Zealand");
+count.set("OM", "Oman");
+count.set("PA", "Panama");
+count.set("PE", "Peru");
+count.set("PF", "French Polynesia");
+count.set("PG", "Papua New Guinea");
+count.set("PH", "Philippines");
+count.set("PK", "Pakistan");
+count.set("PL", "Poland");
+count.set("PM", "Saint Pierre and Miquelon");
+count.set("PN", "Pitcairn");
+count.set("PR", "Puerto Rico");
+count.set("PS", "Palestine, State of");
+count.set("PT", "Portugal");
+count.set("PW", "Palau");
+count.set("PY", "Paraguay");
+count.set("QA", "Qatar");
+count.set("RE", "Réunion");
+count.set("RO", "Romania");
+count.set("RS", "Serbia");
+count.set("RU", "Russian Federation");
+count.set("RW", "Rwanda");
+count.set("SA", "Saudi Arabia");
+count.set("SB", "Solomon Islands");
+count.set("SC", "Seychelles");
+count.set("SD", "Sudan");
+count.set("SE", "Sweden");
+count.set("SG", "Singapore");
+count.set("SH", "Saint Helena, Ascension and Tristan da Cunha");
+count.set("SI", "Slovenia");
+count.set("SJ", "Svalbard and Jan Mayen");
+count.set("SK", "Slovakia");
+count.set("SL", "Sierra Leone");
+count.set("SM", "San Marino");
+count.set("SN", "Senegal");
+count.set("SO", "Somalia");
+count.set("SR", "Suriname");
+count.set("SS", "South Sudan");
+count.set("ST", "Sao Tome and Principe");
+count.set("SV", "El Salvador");
+count.set("SX", "Sint Maarten, (Dutch part)");
+count.set("SY", "Syrian Arab Republic");
+count.set("SZ", "Eswatini");
+count.set("TC", "Turks and Caicos Islands");
+count.set("TD", "Chad");
+count.set("TF", "French Southern Territories");
+count.set("TG", "Togo");
+count.set("TH", "Thailand");
+count.set("TJ", "Tajikistan");
+count.set("TK", "Tokelau");
+count.set("TL", "Timor-Leste");
+count.set("TM", "Turkmenistan");
+count.set("TN", "Tunisia");
+count.set("TO", "Tonga");
+count.set("TR", "Turkey");
+count.set("TT", "Trinidad and Tobago");
+count.set("TV", "Tuvalu");
+count.set("TW", "Taiwan, Province of China");
+count.set("TZ", "Tanzania, United Republic of");
+count.set("UA", "Ukraine");
+count.set("UG", "Uganda");
+count.set("UM", "United States Minor Outlying Islands");
+count.set("US", "United States of America");
+count.set("UY", "Uruguay");
+count.set("UZ", "Uzbekistan");
+count.set("VA", "Holy See");
+count.set("VC", "Saint Vincent and the Grenadines");
+count.set("VE", "Venezuela, Bolivarian Republic of");
+count.set("VG", "Virgin Islands, British");
+count.set("VI", "Virgin Islands, U.S.");
+count.set("VN", "Viet Nam");
+count.set("VU", "Vanuatu");
+count.set("WF", "Wallis and Futuna");
+count.set("WS", "Samoa");
+count.set("YE", "Yemen");
+count.set("YT", "Mayotte");
+count.set("ZA", "South Africa");
+count.set("ZM", "Zambia");
+count.set("ZW", "Zimbabwe");
+
+//////////////////////////////////////////////////////////////
+//..............MAKING AJAX CALLS WITH FETCH FOR SOME DATA
+//kel to cel formula (kelvin - 273.15)
+//var temp=data['main']['temp'] temoerat
+//
+//$("input").css({"border-color":"rgb(235, 232, 232)",'box-shadow': 'none'});
+
+window.setTimeout(() => { }, 2000);
+
+//Menu
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.tooltipped').tooltip();
+  $('.carousel').carousel({
+    indicators: true
+  });
+});
+
+async function call() {
+  var now = new Date();
+  $('.time').text(now.toLocaleTimeString());
+  var temp, convToCel, convToFar, city, country;
+  const user_input = $("#user-input").val();
+  if (user_input == "") {
+    $(".temp").html("00");
+    $('.faren').html('00')
+    $('.city-name').html('XXX');
+    $('.country-name').html("XXX");
+    $('.time').html('00:00:00');
+    $(".weather").html("XXX");
+  }
+  else {
+    try {
+      let res = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${user_input}&appid=373fbc6e4bf925e474d8ef40ad84bf31`)).json();
+            temp = parseFloat(res['main']['temp']);
+            convToCel = temp - 273.15;      //convert to kelvin
+            convToFar = (convToCel * (9 / 5)) + 32;
+            city = res['name'];
+            country = res['sys']['country'];
+            $(".temp").html(convToCel.toFixed(2));
+            $('.faren').html(convToFar.toFixed(2));
+            $('.city-name').html(city);
+            $(".weather").html(res['weather'][0]['description']);
+            $('.country-name').html(count.get(country));
+            console.log(res);
+
+            let res2 = await (await fetch(`https://community-open-weather-map.p.rapidapi.com/forecast?q=${city}`, {
+              "method": "GET",
+              "headers": {
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+                "x-rapidapi-key": "5730203037msh02300bf052bc32bp160c31jsnc228e6a008bd"
+              }
+            })).json();
+
+      let futureWeather = res2['list'];
+      futureWeather.forEach((val, indx) => {
+        let futureDate = val['dt_txt'].split(" ")[0];
+        let futureTime = val['dt_txt'].split(" ")[1];
+        let futureTemp = parseFloat(val['main']['temp']);
+        let futureDesp = val['weather'][0]['description'];
+      
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let dayName = new Date(futureDate);
+        let futureDay = days[dayName.getDay()];
+      
+        let futureConvToCel = futureTemp - 273.15;      //convert to kelvin
+        let futureConvToFar = (futureConvToCel * (9 / 5)) + 32;
+      
+        const markup = `<div class="card row">
+              <div class="card-content white-text">
+                <span class="future-time">Day: <span class="futureValues">${futureDay}</span>
+                <p>
+                Date: <span class="futureValues">${futureDate}</span></span>
+                </p>
+                <p>
+                Time: <span class="futureValues">${futureTime}</span></span>
+                </p>
+                <p class="future-temp">Temperature: <span class="future-cent"><span class="futureValues">${parseInt(futureConvToCel)}&deg;C/${parseInt(futureConvToFar)}&deg;F</span>
+                </p>
+                <p class="future-desp">Sky Description: <span class="futureValues">${futureDesp}</span>
+                </p>
+              </div>
+             </div>`
+        $('.cards-container').append(markup);
+      });
+    }
+    catch (error) {
+      console.log("error", error);
+    };
+  }
+};
+call(); //call the above func
+
+
+
+
+let lst = [
+  {
+    dt_txt: "2020-09-10 12:00:00",
+    main: { temp: 300.59 },
+    weather: [
+      { description: "clear sky is here" },
+      { main: "Clear" }
+    ]
+  },
+  {
+    dt_txt: "2020-09-11 21:00:00",
+    main: { temp: 300.59},
+    weather: [
+      { description: "dpesster clouds" },
+      { main: "dirt" }
+    ]
+  },
+  {
+    dt_txt: "2020-09-09 21:00:00",
+    main: { temp: 300.59 },
+    weather: [
+      { description: "may be raining" },
+      { main: "zameen" }
+    ]
+  },
+  {
+    dt_txt: "2020-09-09 21:00:00",
+    main: { temp: 300.59 },
+    weather: [
+      { description: "huge clouds" },
+      { main: "zameen" }
+    ]
+  }
+];
+
+// lst.forEach((val, indx) => {
+//   let futureDate = val['dt_txt'].split(" ")[0];
+//   let futureTime = val['dt_txt'].split(" ")[1];
+//   let futureTemp = parseFloat(val['main']['temp']);
+//   let futureDesp = val['weather'][0]['description'];
+
+//   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//   let dayName = new Date(futureDate);
+//   let futureDay = days[dayName.getDay()];
+
+//   let futureConvToCel = futureTemp - 273.15;      //convert to kelvin
+//   let futureConvToFar = (futureConvToCel * (9 / 5)) + 32;
+
+//   const markup = `<div class="card blue-grey darken-1">
+//         <div class="card-content white-text">
+//           <span class="future-time">Day: <span class="futureValues">${futureDay}</span>
+//           <p>
+//           Date: <span class="futureValues">${futureDate}</span></span>
+//           </p>
+//           <p>
+//           Time: <span class="futureValues">${futureTime}</span></span>
+//           </p>
+//           <p class="future-temp">Temperature: <span class="future-cent"><span class="futureValues">${parseInt(futureConvToCel)}&deg;C/${parseInt(futureConvToFar)}&deg;F</span>
+//           </p>
+//           <p class="future-desp">Description: <span class="futureValues">${futureDesp}</span>
+//           </p>
+//         </div>
+//        </div>`
+//   $('.cards-container').append(markup);
+// });
+
+console.log(lst[0]);
